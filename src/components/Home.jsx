563432c827bb47732cb0333f0699aae5f924aa08
Ravi Kumar <rav7179@gmail.com>
2025-03-07 19:30:25 +0530

    minor change

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,7 @@ import { TiSocialGithubCircular } from "react-icons/ti";
 import { CiLinkedin } from "react-icons/ci";
 import { CiFacebook } from "react-icons/ci";
 import Typewriter from "typewriter-effect";
+import { NavLink } from "react-router-dom";
 
 function Home() {
   return (
@@ -29,6 +30,11 @@ function Home() {
         engaging user experiences with interactive elements, smooth transitions,
         and eye-catching animations.
       </p>
+
+      <NavLink className={"ubtn"} to={"/projects"}>
+        My Works{" "}
+      </NavLink>
+
       {/* <div className="social-icons">
         <a href="https://github.com/nerdyRK" title="Github" target="_blank">
           <TiSocialGithubCircular />
@@ -44,6 +50,9 @@ function Home() {
           <CiFacebook />
         </a>
       </div> */}
+      <p style={{ position: "absolute", bottom: "10px" }}>
+        &copy; 2025 Abhinav Jindal. All rights reserved
+      </p>
     </div>
   );
 }
